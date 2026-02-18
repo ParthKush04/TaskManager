42040bf5c23af34259f8137959011d55f39e41a1
Parth Kushwaha <parthkush1000@gmail.com>
2026-02-18 11:56:42 +0530

    url changes

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: https://taskmanager-wqs7.onrender.com,
+  baseURL: "https://taskmanager-wqs7.onrender.com",
 });
 
 API.interceptors.request.use((req) => {
